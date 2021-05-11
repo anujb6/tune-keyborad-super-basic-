22062d97d4435d52274c461ddb07422571ab344d
anuj <74948201+anujb6@users.noreply.github.com>
2021-05-11 12:26:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+ const sounds = ['flute', 'orchestra', 'pulse', 'trumpet'];
+
+ sounds.forEach(sound => {
+     const btn = document.createElement('button');
+     btn.classList.add('btn');
+
+     btn.innerText = sound;
+
+     btn.addEventListener('click', () =>{
+        stopSongs();
+
+
+         document.getElementById(sound).play();
+     });
+     
+
+     document.getElementById('buttons').appendChild(btn);
+ })
+
+ function stopSongs(){
+    sounds.forEach(sound =>{
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+
+ }
